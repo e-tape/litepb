c5bffc3a6078787efd7c5a8a51a385213b6be944
Artem Yadelskyi <mymmrac@gmail.com>
2024-06-02 20:41:06 +0300

    Fixed snake case to pascal case

--- pkg/common/case.go
+++ pkg/common/case.go
@@ -9,15 +9,21 @@ import (
 // SnakeCaseToPascalCase converts snake_case to PascalCase
 func SnakeCaseToPascalCase(text string) string {
 	data := []byte(strings.ToLower(text))
-	data[0] -= 32
+	if isLowerCaseLetter(data[0]) {
+		data[0] -= 32
+	}
 
 	i := bytes.IndexByte(data, '_')
 	for ; i >= 0; i = bytes.IndexByte(data, '_') {
-		if data[i+1] >= 'a' && data[i+1] <= 'z' || data[i+1] >= 'A' && data[i+1] <= 'Z' {
+		if isLowerCaseLetter(data[i+1]) {
 			data[i+1] -= 32
 		}
 		data = slices.Delete(data, i, i+1)
 	}
 
 	return string(data)
+}
+
+func isLowerCaseLetter(c byte) bool {
+	return c >= 'a' && c <= 'z'
 }
